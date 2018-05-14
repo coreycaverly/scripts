fc851154d831f76cec15600dd605b4dceb8e45fc
Corey Caverly <corey_caverly@intuit.com>
2018-05-14 16:01:35 -0700

    For those of us who forget to lock our computers

---
+++ unlocked.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+usage="[01;32m$(basename "$0") [-d]
+
+where: 
+  -h              Dispaly this help message
+  -d              Daemon Mode
+[04;34m\n[00m"
+
+while getopts ':h:d' option; do
+  case "$option" in
+    h) echo "$usage"
+       exit
+       ;;
+    d) daemon=TRUE
+       ;;
+   \?) printf "[01;31millegal option: -%s\n[00m" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+start_volume=`osascript -e 'get volume settings'|awk -F',' '{print \$1}'|awk -F':' '{print \$2}'`
+desired_volume=5
+sayings=("I really should lock my computer"\ 
+    "I got hacked because I have a lack of technical skillz"\ 
+    "I just farted" "why is everybody looking at me"\ 
+    "I voted for Donald Trump"\ 
+    "my imaginary friends tell me that i have a certain jen a say qua"\ 
+    "I like tacos")
+
+dv="osascript -e \"set volume output volume ${desired_volume}\""
+sv="osascript -e \"set volume output volume ${start_volume}\""
+st="sleep $[ ($RANDOM % 10 ) + 1 ]s"
+
+if [[ ${daemon} ]]; then
+    while true; do
+        #echo "Setting to my volume ${dv}"
+        eval ${dv}
+        st="sleep $[ ($RANDOM % 10 ) + 1 ]s";
+        selected=${sayings[$RANDOM % ${#sayings[@]} ] }
+        #echo "${selected}"
+        say "${selected}"
+        #echo "Setting to original volume ${sv}"
+        eval ${sv}
+        #echo ${st}
+        eval ${st}
+    done
+else
+    #echo "Setting to my volume ${dv}"
+    eval ${dv}
+    selected=${sayings[$RANDOM % ${#sayings[@]} ] }
+    #echo "${selected}"
+    say "${selected}"
+    #echo "Setting to original volume ${sv}"
+    eval ${sv}
+fi
+
+
